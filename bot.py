58115094b0464d1b95102f150d536b6e90fbc4fa
juanjimenez89 <bruan18@gmail.com>
2026-03-30 21:31:32 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,19 @@
 import time
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import threading
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot activo")
+
+def run_server():
+    port = 10000
+    server = HTTPServer(('', port), Handler)
+    server.serve_forever()
+
+threading.Thread(target=run_server).start()
 
 print("Amazon bot iniciado")
 
